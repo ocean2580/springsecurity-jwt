5c0f8a93e0829814f85e6ea72671757612edb430
Ocean <2080917477@qq.com>
2022-09-13 15:50:51 +0800

    数据库校检用户

---
+++ src/main/java/com/ocean/service/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,37 @@
+package com.ocean.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.ocean.entity.LoginUser;
+import com.ocean.entity.User;
+import com.ocean.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        // 查询用户信息
+        LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(User::getUserName,username);
+        User user = userMapper.selectOne(queryWrapper);
+        if (Objects.isNull(user)) {
+            throw new RuntimeException("用户名或密码错误");
+        }
+
+        //TODO 查询对应权限信息
+
+
+        return new LoginUser(user);
+    }
+}
